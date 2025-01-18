b6a070dcff188a9c4aa4716e37a1d7bb3b1647ce
HoreaMatei <mateihorea17@yahoo.com>
2025-01-18 16:08:40 +0200

    cors changes

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 const cors = require("cors");
-app.use(cors());
+app.use(cors({ origin: "https://lord-121g.vercel.app" }));
 
 connectDB();
 
