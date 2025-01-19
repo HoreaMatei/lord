7d242c8c6379cccc20bc23563d4dba455b9079d0
HoreaMatei <mateihorea17@yahoo.com>
2025-01-19 16:20:51 +0200

    port changes

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 const cors = require("cors");
-app.use(cors({ origin: PORT }));
+app.use(cors({ origin: process.env.FRONTEND_PORT }));
 
 connectDB();
 
